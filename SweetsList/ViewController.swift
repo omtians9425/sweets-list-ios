713af4e874628cdb2d94d5cba5f0880c7ccc6822
omtians9425 <rskowgshakhtardonetsk32517@gmail.com>
2020-01-02 20:08:32 +0900

    Initial Commit

---
+++ SweetsList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SweetsList
+//
+//  Created by 小俣幸之助 on 2020/01/02.
+//  Copyright © 2020 Konosuke-o. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
